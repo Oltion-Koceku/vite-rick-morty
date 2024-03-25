de9a1d4b59ab40de2bfb0b4c10bafffccd2864a4
Oltion-Koceku <koceku17@gmail.com>
2024-03-25 15:49:01 +0100

    [add]api and card

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,18 +1,45 @@
 <script>
 import {store} from '../data/store'
-  export default {
+import mainCards from './partials/mainCards.vue';
+import  axios  from 'axios';
+
+  export default{
     data(){
       return{
-        store
+        store,
+        axios
       }
+    },
+
+    methods:{
+      apiCall(){
+        this.axios.get(this.store.apiURl)
+          .then(res  =>{
+            this.store.apiArray = res.data.results
+          })
+
+          .catch(error =>{
+            console.log("ERRORRREEEEEEE");
+          })
+      }
+    },
+
+    mounted(){
+      this.apiCall()
+    },
+
+    components:{
+      mainCards
     }
   }
 </script>
 
 
 <template>
-  <div>
-    
+  <div class="container">
+    <mainCards 
+      :arrayApi = "this.store.apiArray"
+    />
   </div>
 </template>
 
---
+++ src/components/partials/mainCards.vue
@@ -0,0 +1,30 @@
+<script>
+  export default {
+    props:{
+      arrayApi: Array
+    },
+
+    mounted(){
+      console.log(this.arrayApi);
+    }
+  }
+</script>
+
+<template>
+  
+    <div class="row row-cols-4 ">
+      <div v-for="api in arrayApi" :key="api.id" class="card m-2 " style="width: 18rem;">
+        <img :src="api.image" class="card-img-top" :alt="api.name">
+        <div class="card-body">
+          <h5 class="card-title">{{api.name}}</h5>
+          <p class="card-text">{{api.status}}</p>
+          <p>{{api.species}}</p>
+        </div>
+      </div>
+    </div>
+  
+</template>
+
+<style lang="scss" scoped>
+
+</style>
