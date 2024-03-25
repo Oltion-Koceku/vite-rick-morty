567af7037e00ead27b23c227259f9786569f0c30
Oltion Koceku <152490062+Oltion-Koceku@users.noreply.github.com>
2024-03-25 14:30:57 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import Header from './components/Header.vue'
+import Main from './components/Main.vue'
+import Footer from './components/Footer.vue'
+  export default {
+    components:{
+      Header,
+      Main,
+      Footer
+    }
+  }
+</script>
+
+<template>
+  <Header />
+  <Main />
+</template>
+
+
+
+<style lang="scss" scoped>
+@use './assets/scss/main.scss';
+</style>
